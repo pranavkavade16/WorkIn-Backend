9a7f8c1376205361596bba6f4da986bfaf84d9d1
pranavkavade16 <pranavkavade69739@gmail.com>
2026-04-09 12:45:08 +0530

    fixed the middleware errors

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -9,15 +9,18 @@ const verifyJWT = (req, res, next) => {
 
   const token = authHeader.split(" ")[1];
 
+  if (!token) {
+    return res.status(401).json({ message: "Malformed token." });
+  }
+
   try {
     const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decodedToken;
-
     next();
   } catch (error) {
     return res
-      .status(402)
-      .json({ message: "Invalid token", error: error.message });
+      .status(403)
+      .json({ message: "Invalid token", error: error.message }); // ✅ 403, not 402
   }
 };
 
